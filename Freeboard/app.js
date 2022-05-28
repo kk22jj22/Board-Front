7e5ba948317fb4a44481b1088f89b14c52ed9d01
jwkim <rksrur128@naver.com>
2022-05-29 03:11:06 +0900

    게시물 등록 페이지 추가
    cate1
    newpost

--- Freeboard/app.js
+++ Freeboard/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var cate1Router = require('./routes/cate1');
+var newpostRouter = require('./routes/newpost');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/cate1', cate1Router);
+app.use('/newpost', newpostRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
