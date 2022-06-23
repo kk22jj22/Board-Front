f378b1691d50710489160c5f2c348759b6a9c3a7
김지원 <rksrur128@naver.com>
2022-06-23 16:49:21 +0900

    Add  1. 카테고리 구분 추가
    Add
    1. 카테고리 구분 추가

--- Freeboard/public/javascripts/views/category.js
+++ Freeboard/public/javascripts/views/category.js
@@ -21,6 +21,8 @@ function newPost() {
             toLocation('/newpost?cateid=1')
         } else if(location.search === '?cateid=2') {
             toLocation('/newpost?cateid=2')
+        } else if(location.search === '?cateid=3') {
+            toLocation('/newpost?cateid=3')
         }
     }
 }
@@ -38,43 +40,52 @@ async function getPostLists() {
     let pageNo = 1
     let numsOfPages = 20
     let category = getCurrentCategory()
+    let BoardNoCnt = 1
 
     const getPostLists = await new getPostController().getPost(pageNo, numsOfPages, category)
     const trlength = getPostLists.list.length
-    const tdlength = Object.keys(getPostLists.list[0]).length
-      
-    for(let i=0; i<trlength; i++) {
-        const body = getById('post-list-body')
-        const tr = document.createElement('tr')
-
-        let tdBoardId = document.createElement('td')
-        tdBoardId.textContent = getPostLists.list[i].boardId
-
-        let tdTitle = document.createElement('td')
-        tdTitle.textContent = getPostLists.list[i].title 
-
-        let tdCommentCount = document.createElement('td')
-        tdCommentCount.textContent = getPostLists.list[i].commentCount 
-        
-        let tdViews = document.createElement('td')
-        tdViews.textContent = getPostLists.list[i].views
-
-        let tdDate = document.createElement('td')
-        tdDate.textContent = getPostLists.list[i].date
-
-        let tdNickName = document.createElement('td')
-        tdNickName.textContent = getPostLists.list[i].nickName
-
-        tr.appendChild(tdBoardId)
-        tr.appendChild(tdTitle)
-        tr.appendChild(tdCommentCount)
-        tr.appendChild(tdViews)
-        tr.appendChild(tdDate)
-        tr.appendChild(tdNickName)
-
-        body.appendChild(tr)
-    }
-
-
+    // const tdlength = Object.keys(getPostLists.list[0]).length
     
+    // 추가 작업 필요 : boardNo 수정, 페이지네이션, 작성일자 > 시간 자르기, 상세 이동
+    for(let i=0; i<trlength; i++) {
+        if(getPostLists.list[i].category === category) {
+            const body = getById('post-list-body')
+            const tr = document.createElement('tr')
+    
+            // BoardNoCnt = BoardNoCnt+1
+            // if(BoardNoCnt>=2) {
+            //     for(let j=BoardNoCnt; j>1; j--) {
+
+            //     }               
+            // }
+
+            let tdBoardNo = document.createElement('td')
+            tdBoardNo.textContent = BoardNoCnt
+
+            BoardNoCnt = BoardNoCnt+1
+    
+            let tdTitle = document.createElement('td')
+            tdTitle.textContent = getPostLists.list[i].title 
+    
+            let tdCommentCount = document.createElement('td')
+            tdCommentCount.textContent = getPostLists.list[i].commentCount 
+            
+            let tdViews = document.createElement('td')
+            tdViews.textContent = getPostLists.list[i].views
+    
+            let tdDate = document.createElement('td')
+            tdDate.textContent = getPostLists.list[i].date
+    
+            let tdNickName = document.createElement('td')
+            tdNickName.textContent = getPostLists.list[i].nickName
+    
+            tr.appendChild(tdBoardNo)
+            tr.appendChild(tdTitle)
+            tr.appendChild(tdCommentCount)
+            tr.appendChild(tdViews)
+            tr.appendChild(tdDate)
+            tr.appendChild(tdNickName)   
+            body.appendChild(tr)
+        }
+    }
 }
